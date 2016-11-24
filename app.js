97a654ac715e760f4d7a0127526894b9d55d9af3
David Kananda <kd43@gre.ac.uk>
2016-11-24 16:10:38 +0000

    set up basic server and dependencies
    This includes app.js and modules: express, ejs, mocha, should.
    Also gitignored npm-debug.log.

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/', function (req, res, next) {
+	console.log('Basic server up and running.');
+	next();
+});
+
+app.listen(port);

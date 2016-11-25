0a878e46fd85395289fa33e0681aa808db2445d2
David Kananda <kd43@gre.ac.uk>
2016-11-25 18:29:02 +0000

    Mocking the API end point in listContoller test
    This includes creating a sample listingResponse json and added nock package for mocking custom responses to HTTP requests for the test.

--- test/listingController.test.js
+++ test/listingController.test.js
@@ -1,7 +1,16 @@
 var expect = require('chai').expect;
+var nock = require('nock');
 var getListings = require('../controllers/listingController').getListings;
+var listingResponse = require('./listingResponse');
 
 describe('GET listing from API end point', () => {
+  beforeEach(()=>{   
+    // mock the api end point request response
+    nock('https://ibl.api.bbci.co.uk')
+      .get('/ibl/v1/atoz/a/programmes?page=1')
+      .reply(200, listingResponse);
+  });
+  
   it('should return listings', (done) => {
     
     // mock request: req and response: res objects
@@ -19,6 +28,7 @@ describe('GET listing from API end point', () => {
     getListings(req, res, (err, listings) => {     
       expect(listings).to.have.property('atoz_programmes');
       expect(Array.isArray(listings.atoz_programmes.elements)).to.equal(true);
+      expect(listings.atoz_programmes.elements).to.have.lengthOf(1);
       done();
     });
 

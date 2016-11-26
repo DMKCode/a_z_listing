240034cc0a088e0f601c4fda56fa589a15d3f2f7
David Kananda <kd43@gre.ac.uk>
2016-11-26 23:52:51 +0000

    Pass 404 test
    Added middleware to handle 404 in the app module.

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use('/', listingRoutes);
 
+// Handle 404
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404', { title: '404 Page Not Found.'} );
+});
+
 
 app.listen(port);
 

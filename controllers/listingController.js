ff14f8ddee2317624d060ca938368e1946848237
David Kananda <kd43@gre.ac.uk>
2016-11-27 19:02:37 +0000

    Pass test for letter is 0-9
    Included check in listingController module.

--- controllers/listingController.js
+++ controllers/listingController.js
@@ -4,7 +4,7 @@ var getListings = (req, res, callback) => {
     var letter = req.params.letter;
     var pageNum = req.query.page || 1; // API defaults to page 1
 
-    if(!singleAtoZ(letter)) {
+    if(!singleAtoZ(letter) && !zeroToNine(letter)) {
         callback('404');
         return;
     }
@@ -28,14 +28,19 @@ var getListings = (req, res, callback) => {
 
 var singleAtoZ = (letter) => {
     return /^[a-z]$/.test(letter);
-}
+};
+
+var zeroToNine = (zeroNine) => {
+    return /^0-9$/.test(zeroNine);
+};
 
 var numberFrom1 = (num) => {
     return /^[1-9]+$/.test(num);
-}
+};
 
 module.exports = { 
     getListings: getListings,
     singleAtoZ: singleAtoZ,
+    zeroToNine: zeroToNine,
     numberFrom1: numberFrom1 
 };

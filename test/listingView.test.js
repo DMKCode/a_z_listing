1815b2da8994a917650e3b91681355fc455c55b8
David Kananda <kd43@gre.ac.uk>
2016-11-28 01:49:38 +0000

    Pass test for show letter navigations
    This included refactoring of tests in the listingView tet suite.
    
    Also updated listingView view.

--- test/listingView.test.js
+++ test/listingView.test.js
@@ -41,40 +41,15 @@ describe('Listing Routes', () => {
         it('should show page listings: image and title', (done) => {
             assert.ok(this.browser.success);
             this.browser.assert.text('h1', 'A-Z Programme Listing');
-            this.browser.assert.text('ul li', listingResponse.atoz_programmes.elements[0].title);
-            this.browser.assert.attribute('ul li img', 'src', (listingResponse.atoz_programmes.elements[0].images.standard).replace("{recipe}", "560x315"));
+            this.browser.assert.text('ol li', listingResponse.atoz_programmes.elements[0].title);
+            this.browser.assert.attribute('ol li img', 'src', (listingResponse.atoz_programmes.elements[0].images.standard).replace("{recipe}", "560x315"));
             done();
         });
 
         it('should show letter navigations', (done) => {
             assert.ok(this.browser.success);
             this.browser.assert.text('h1', 'A-Z Programme Listing');
-            this.browser.assert.text('#letter-navigation ul li a', 'A');
-            this.browser.assert.text('#letter-navigation ul li a', 'B');
-            this.browser.assert.text('#letter-navigation ul li a', 'C');
-            this.browser.assert.text('#letter-navigation ul li a', 'D');
-            this.browser.assert.text('#letter-navigation ul li a', 'E');
-            this.browser.assert.text('#letter-navigation ul li a', 'F');
-            this.browser.assert.text('#letter-navigation ul li a', 'G');
-            this.browser.assert.text('#letter-navigation ul li a', 'H');
-            this.browser.assert.text('#letter-navigation ul li a', 'I');
-            this.browser.assert.text('#letter-navigation ul li a', 'J');
-            this.browser.assert.text('#letter-navigation ul li a', 'K');
-            this.browser.assert.text('#letter-navigation ul li a', 'L');
-            this.browser.assert.text('#letter-navigation ul li a', 'M');
-            this.browser.assert.text('#letter-navigation ul li a', 'N');
-            this.browser.assert.text('#letter-navigation ul li a', 'O');
-            this.browser.assert.text('#letter-navigation ul li a', 'P');
-            this.browser.assert.text('#letter-navigation ul li a', 'Q');
-            this.browser.assert.text('#letter-navigation ul li a', 'R');
-            this.browser.assert.text('#letter-navigation ul li a', 'S');
-            this.browser.assert.text('#letter-navigation ul li a', 'T');
-            this.browser.assert.text('#letter-navigation ul li a', 'U');
-            this.browser.assert.text('#letter-navigation ul li a', 'V');
-            this.browser.assert.text('#letter-navigation ul li a', 'W');
-            this.browser.assert.text('#letter-navigation ul li a', 'X');
-            this.browser.assert.text('#letter-navigation ul li a', 'Y');
-            this.browser.assert.text('#letter-navigation ul li a', 'Z');
+            this.browser.assert.text('#letter-navigation ul li a', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0-9');
             done();
         });
     });

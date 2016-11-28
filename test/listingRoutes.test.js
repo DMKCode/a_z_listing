9fa16c7372085e853206b421dc588ca962d4445f
David Kananda <kd43@gre.ac.uk>
2016-11-28 13:01:17 +0000

    Added test to re-direct from home - FAIL
    Includes re-direct to the letter a, page 1 as the default.

--- test/listingRoutes.test.js
+++ test/listingRoutes.test.js
@@ -22,7 +22,7 @@ describe('Listing Routes', () => {
     done();
   });
 
-  describe('GET /', () => {
+  describe('GET Rendering', () => {
     it('should render the listing view', (done) => {
       chai.request(app)
         .get('/a/programmes')
@@ -41,6 +41,18 @@ describe('Listing Routes', () => {
           res.status.should.equal(404);
           res.type.should.equal('text/html');
           res.text.should.contain('<h1>404 Page Not Found.</h1>');
+          done();
+        });
+    });
+
+    it('should re-direct to default page: a-z(home) to a, page 1', (done) => {
+      chai.request(app)
+        .get('/a-z')
+        .end((err, res) => {
+          res.status.should.equal(200);
+          res.redirects.length.should.equal(1);
+          res.type.should.equal('text/html');
+          res.text.should.contain('<h1>A-Z Programme Listing</h1>');
           done();
         });
     });
